323eb62e4d10d4fd1e483aacae949295caad3be9
Sandy Hung <sandyhung83@yahoo.com.tw>
2023-02-11 19:08:07 +1100

    added data in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (id, name)
+VALUES ( , "Sales"),
+       ( , "Legal"),
+       ( , "Engineering"),
+       ( , "Finance");
+
+INSERT INTO role (id, title, salary, )
+VALUES ( , "Sales Lead"),
+       ( , "Salesperson"),
+       ( , "Lead Engineer"),
+       ( , "Software Engineer");
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES ( , "John", "Doe",),
+       ( , "Mike", "Chan"),
+       ( , "Ashley", "Rodriguez"),
+       ( , "Kevin", "Tupik");
